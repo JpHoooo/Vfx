2555a954e990fb79fbac5ec4ffe8dea139b04ee5
JpHoooo <156303815@qq.com>
2020-06-09 17:31:45 +0800

    visual effect graph

---
+++ Assets/FlameThrower/Scripts/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public GameObject pivot;
+    public float rotateSpeed= 20;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        if(Input.GetKey(KeyCode.A))
+        {
+            pivot.transform.Rotate(new Vector3(0, -rotateSpeed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            pivot.transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
+        }
+    }
+}
